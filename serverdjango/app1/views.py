dd6a933e51fb285db127fd1e9b267e7cd0cbd72f
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-10-30 04:53:57 +1100

    Add page app1

--- serverdjango/app1/views.py
+++ serverdjango/app1/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Agency
+from .models import Calendar
+from .models import CalendarDates
+from .models import Routes
+from .models import Shapes
+from .models import StopTimes
+from .models import Stops
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
