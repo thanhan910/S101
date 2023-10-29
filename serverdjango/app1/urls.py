e20df6c1c828e1b94fd8ac80fded6335d984bbcc
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-10-30 07:49:19 +1100

    Implemented templates

--- serverdjango/app1/urls.py
+++ serverdjango/app1/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("agency/<str:agency_id>", views.agency, name="agency"),
 ]
--- serverdjango/app1/views.py
+++ serverdjango/app1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Agency
 from .models import Calendar
 from .models import CalendarDates
@@ -15,3 +15,10 @@ from .models import Stops
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def agency(request, agency_id):
+    agency = get_object_or_404(Agency, agency_id=agency_id)
+    context = {
+        "agency": agency
+    }
+    return render(request, "app1/agency.html", context)
